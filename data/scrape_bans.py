8f0d9995d3782f5c6781d4a9be0e6c01c28acdc2
Hange Zoe <skylervestal@utexas.edu>
2020-12-10 05:01:45 -0600

    Webscrapes to extract individual match lists

--- data/scrape_bans.py
+++ data/scrape_bans.py
@@ -9,39 +9,54 @@ def main():
     # Dictionary for player names -> player data (could be list -- not sure)
     player_col = {}
     for file_name in glob(DATA_PATH):
-        get_player_name(open(file_name, "r"))
+        gen_player(file_name)
         # player = gen_player(file_name)
         # player_col[player.name] = player
 
 
 # Generates and returns a single player object from a file
 def gen_player(file_name):
-    f = open(file_name, "r")
-    player_name = get_player_name(f)
-    match_list = get_match_list(f)
-    f.close()
-    return Player(player_name, match_list)
+    player_name = get_player_name(file_name)
+    match_list = get_match_list(file_name)
+    #return Player(player_name, match_list)
 
 
 # Parse file for main name of player at top of file
-def get_player_name(file):
+def get_player_name(file_name):
     # Parse only tag w/ white name at top of page
-    soup = bs(file, 'html5lib')
-    name = soup.find("a", {"class" : "whiteLink persona_name_text_content"})
-    # Strip tabbing(?) stuff off name
-    return name.text.strip()
+    with open(file_name, "r") as file:
+        soup = bs(file, 'html5lib')
+        name = soup.find("a", {"class" : "whiteLink persona_name_text_content"})
+        # Strip tabbing(?) stuff off name
+        return name.text.strip()
 
 
 # Parses a file and returns a list of match objects
-def get_match_list(file):
-    soup = bs(file, 'html5lib')
-    raw_matches = soup.find("")
+def get_match_list(file_name):
+    match_list = []
+    with open(file_name, "r") as file:
+        soup = bs(file, 'html5lib')
+        # Find main table contaning all map and player data (the darker blue panel)
+        raw_all_table = soup.find("table", {"class": "generic_kv_table csgo_scoreboard_root"})
+        # It should exist -- probably renamed if this fails
+        assert raw_all_table is not None
+        # This part is super awkward to parse due to the chrome extension not making a new tbody nor appending
+        # to the current tbody. I'll just create two lists instead even if it's a little gacky
+        match_map_list = [x for x in raw_all_table.find_all("td", {"class": "val_left banchecker-counted"})]
+        match_players_list = [x for x in raw_all_table.find_all("table", {"class": "csgo_scoreboard_inner_right banchecker-formatted banchecker-withcolumn"})]
+        # Should have the same num of map data and player data tables
+        assert len(match_map_list == match_players_list)
 
+        # Now make a new match for each game
+        for index in len(match_map_list):
+            new_match = Match(match_map_list[index], match_players_list[index])
+            match_list.append(new_match)
+    return match_list
 
 # Class for a single match data -- just a container but makes it easier to hold
 class Match:
 
-    def __init__(self, data):
+    def __init__(self, map_data, match_data):
         return 0
 
 
